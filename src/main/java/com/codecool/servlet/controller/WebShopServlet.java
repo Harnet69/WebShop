b21a94b14b1371bb421c7157368189f584bc25c7
Harnet69 <harnet69@gmail.com>
2020-03-16 10:44:34 +0100

    Items show now on the webshop page

--- src/main/java/com/codecool/servlet/controller/WebShopServlet.java
+++ src/main/java/com/codecool/servlet/controller/WebShopServlet.java
@@ -2,30 +2,23 @@ package com.codecool.servlet.controller;
 
 import com.codecool.servlet.model.Item;
 import com.codecool.servlet.model.Stock;
-import org.w3c.dom.ls.LSOutput;
 
 import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Iterator;
+import java.util.Set;
 
 @WebServlet(name = "webShopServlet", urlPatterns = {"/webshop"}, loadOnStartup = 1)
 public class WebShopServlet extends HttpServlet {
+    Stock stock = Stock.getInstance();
 
     public void init() throws ServletException {
-        System.out.println("Initialization was done");
-        Stock stock = Stock.getInstance();
         stock.addDefaultItemsToStock();
-//        System.out.println(stock.getItemsInStock());
-        stock.showItemsInStock();
         stock.addItemToStock(new Item("Toilet paper", 0.99));
-        System.out.println();
-        stock.showItemsInStock();
     }
 
     @Override
@@ -33,17 +26,21 @@ public class WebShopServlet extends HttpServlet {
 
         PrintWriter out = response.getWriter();
         String title = "WebShop";
+        Set<Item> items = Stock.getInstance().getItemsInStock();
 
         out.println(
                 "<html>\n" +
                         "<head><title>" + title + "</title></head>\n" +
                         "<body>\n" +
-                        "<h1 align = \"center\">" + title + "</h1>\n" +
-                        "<ul>\n" +
-                        "<li><b>First Name</b>: " + request.getParameter("first_name") + "\n" +
-                        "<li><b>Last Name</b>: " + request.getParameter("last_name") + "\n" +
-                        "</ul>\n" +
-                        "<div>Visit another servlet: <a href=\"/another\">Visit the other servlet</a></div>" +
+                        "<a href='/'>Home</a><h1 align = \"center\">" + title + "</h1>\n" +
+                        "<ul>\n"
+        );
+        for (Item item : items) {
+            out.println(
+                    "<li>" + item.getName() + " : " + item.getPrice() + "</li>");
+        }
+        out.println(
+                "</ul>\n" +
                         "</body></html>"
         );
     }
