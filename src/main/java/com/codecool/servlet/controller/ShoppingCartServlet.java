2174e17dcfe9c1f7066316480e86e7b397fad426
Harnet69 <harnet69@gmail.com>
2020-03-16 11:39:30 +0100

    Shopping cart page was created

--- src/main/java/com/codecool/servlet/controller/ShoppingCartServlet.java
+++ src/main/java/com/codecool/servlet/controller/ShoppingCartServlet.java
@@ -12,21 +12,17 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Set;
 
-@WebServlet(name = "webShopServlet", urlPatterns = {"/webshop"}, loadOnStartup = 1)
-public class WebShopServlet extends HttpServlet {
-    Stock stock = Stock.getInstance();
-
+@WebServlet(name = "shoppingCartServlet", urlPatterns = {"/shopping-cart"}, loadOnStartup = 1)
+public class ShoppingCartServlet extends HttpServlet {
     public void init() throws ServletException {
-        stock.addDefaultItemsToStock();
-        stock.addItemToStock(new Item("Toilet paper", 0.99));
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
         PrintWriter out = response.getWriter();
-        String title = "WebShop";
-        Set<Item> items = Stock.getInstance().getItemsInStock();
+        String title = "Shopping Cart";
+//        Set<Item> items = Stock.getInstance().getItemsInStock();
 
         out.println(
                 "<html>\n" +
@@ -35,18 +31,17 @@ public class WebShopServlet extends HttpServlet {
                         "<a href='/'>Home</a><h1 align = \"center\">" + title + "</h1>" +
                         "<table align='center' border='1'> <tr><th>Name</th><th>Price</th><th>Add</th><th>Remove</th></tr>\n"
         );
-        for (Item item : items) {
-            out.println(
-                    "<tr>" +
-                            "<td>" + item.getName() + "</td>" +
-                            "<td>" + item.getPrice()+ " USD" + "</td>" +
-                            "<td>" + "<a href='#'>Add</a>" + "</td>" +
-                            "<td>" + "<a href='#'>Remove</a>" + "</td>" +
-                    "</tr>");
-        }
+//        for (Item item : items) {
+//            out.println(
+//                    "<tr>" +
+//                            "<td>" + item.getName() + "</td>" +
+//                            "<td>" + item.getPrice()+ " USD" + "</td>" +
+//                            "<td>" + "<a href='#'>Add</a>" + "</td>" +
+//                            "<td>" + "<a href='#'>Remove</a>" + "</td>" +
+//                            "</tr>");
+//        }
         out.println(
                 "</table>\n" +
-                       "<a href=\"/shopping-cart\"><h3 align='center'>Shopping cart</h3></a>" +
                         "</body></html>"
         );
     }
